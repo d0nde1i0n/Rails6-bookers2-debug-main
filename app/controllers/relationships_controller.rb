94e4f4d2f30cef19c189ec87bea9d97d3c2539ed
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-23 06:25:32 +0000

    [Update] フォロー/フォロワー機能に対する自分なりの解釈をコメントとして、追記

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,8 @@ before_action :authenticate_user!
   def create
     # ログインユーザidとそのフォロー対象のユーザidを取得し、
     # 変数paramsに格納
+    # (follower_id,fllowed_id) = (ログインユーザid,フォロー対象のユーザid)
+    # フォローするユーザ側から見た時の操作
     relationship = current_user.relationships.new(followed_id: params[:user_id])
     # relationshipsテーブルに必要な情報を全て格納できたため、DBに格納
     relationship.save
@@ -15,9 +17,10 @@ before_action :authenticate_user!
   def destroy
     # ログインユーザidとログインユーザがフォローしているユーザのidを取得し、
     # 変数paramsに格納
+    # (follower_id,fllowed_id) = (ログインユーザid,フォロー対象のユーザid)
     relationship = current_user.relationships.find_by(followed_id: params[:user_id])
-    # relationshipsテーブルのレコードの中から、変数followerに格納されている情報と
-    # 一致するレコードを削除
+    # relationshipsテーブルのレコードの中から、follower_id(ログインユーザid)、
+    # followed_id(フォロー対象のユーザid）が一致するレコードを削除
     relationship.destroy
     # リダイレクト先はリンク元のページを指定
     redirect_to request.referer
